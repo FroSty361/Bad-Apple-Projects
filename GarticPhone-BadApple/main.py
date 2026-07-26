5c228cd877e6b0e1faf5cf1dbdc27902c2b13449
FroSty361 <192673158+FroSty361@users.noreply.github.com>
2026-07-26 16:57:43 -0700

    Added CSV File User Input To GarticPhone-BadApple

--- GarticPhone-BadApple/main.py
+++ GarticPhone-BadApple/main.py
@@ -1,13 +1,53 @@
+import csv
 import sys
-
 from src.badapple_parser import start
+from pathlib import Path
 
 def main():
-    user_input = get_user_input()
+    choice = input("Input CSV {1} Or Input Manual Values {2} ")
+
+    if choice == "1":
+        user_input = get_user_input_csv()
+    elif choice == "2":
+        user_input = get_user_input_manual()
+    else:
+        sys.exit("Must Input {1}} Or {2}")
 
     start(user_input)
 
-def get_user_input() -> dict:
+def get_user_input_csv() -> dict:
+    csv_output = {}
+
+    csv_path = Path(input("Input CSV Path: "))
+
+    if not csv_path.exists():
+        sys.exit(f"CSV Path {csv_path} Does Not Exist")
+
+    with open(csv_path, mode='r', encoding='utf-8') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        for row in csv_reader:
+            csv_output[row[0]] = row[1]
+
+    user_input = {}
+
+    possible_keys = [ "video_path", "width", "height", "start_x", "start_y" ]
+
+    for key, value in csv_output.items():
+        if key not in possible_keys:
+            sys.exit(f"Key {key} Does Not Exist")
+
+        if key == "video_path":
+            user_input["video_path"] = value
+        else: # For Integers
+            try:
+                user_input[key] = int(value)
+            except ValueError:
+                sys.exit(f"{key} Value Must Be An Integer")
+
+    return user_input
+
+def get_user_input_manual() -> dict:
     user_input = {}
 
     video_path = input("Input Video Path: ")
