c9465f26e2842d94b847ef151e3e1404c85a2a22
FroSty361 <192673158+FroSty361@users.noreply.github.com>
2026-07-25 19:52:20 -0700

    Implemented start function of badapple_parser in GarticPhone-BadApple

--- GarticPhone-BadApple/src/badapple_parser.py
+++ GarticPhone-BadApple/src/badapple_parser.py
@@ -1,7 +1,58 @@
 import cv2
-from draw_frame import FrameDrawer
+import sys
+import time
+from .draw_frame import FrameDrawer
+from pathlib import Path
 
-def start(video_path: str):
-    frame_drawer = FrameDrawer()
+def start(video_path_str: str):
+    video_path = Path(video_path_str)
+
+    if not video_path.exists():
+        sys.exit(f"Video Path {video_path} Does Not Exist")
+
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        sys.exit("Could Not Open Video")
+
+    video_properties = get_video_properties(cap)
+    frame_drawer = FrameDrawer(video_properties)
+
+    fps = video_properties["fps"]
+
+    if fps == 0:
+        fps = 30
+
+    print(video_properties["fps"])
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        frame_drawer.draw_frame(frame)
+
+        time.sleep(1000 / fps)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+
+def get_video_properties(cap: cv2.VideoCapture) -> dict:
+    if not cap.isOpened():
+        sys.exit("Can Not Get Video Properties If Video Is Not Open")
+
+    video_properties = {}
+
+    video_properties["width"] = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    video_properties["height"] = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    video_properties["fps"] = int(cap.get(cv2.CAP_PROP_FPS))
+    video_properties["frame_count"] = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    return video_properties
 
     pass
--- GarticPhone-BadApple/src/draw_frame.py
+++ GarticPhone-BadApple/src/draw_frame.py
@@ -1,12 +1,13 @@
 import cv2
 import pyautogui
 
-class FrameDrawer():
-    def __init__(self):
+class FrameDrawer:
+    def __init__(self, video_properties: dict):
         screenwidth, screenheight = pyautogui.size()
 
         self.screenwidth = screenwidth
         self.screenheight = screenheight
 
+        self.video_properties = video_properties
+
     def draw_frame(self, frame):
-        pass
